534eee57f40342a3b5baee7db576bea74783449e
jhon3242 <rldotnr2@gmail.com>
2024-04-22 21:35:30 +0900

    refactor : repository DTO를 사용하는 방식으로 통일

--- src/test/java/roomescape/times/CrudTest.java
+++ src/test/java/roomescape/times/CrudTest.java
@@ -7,13 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
 import roomescape.dto.ReservationTimeDto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD) // TODO 테스트에서 너무 많은 Context를 만드는건 아닐까?
 public class CrudTest {
 
     @Autowired
